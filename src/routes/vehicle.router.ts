3ea86d870822086f8e9edeecc96b096eefc318f1
lmfreire <l.matheus.freire10@gmil.com>
2022-12-18 14:55:27 -0300

    feat/updateVehicle

--- src/routes/vehicle.router.ts
+++ src/routes/vehicle.router.ts
@@ -2,11 +2,13 @@ import { Express } from "express";
 import CreateVehicleController from "../controllers/Vehicle/create.vehicles.controller";
 import DeleteVehicleController from "../controllers/Vehicle/delete.vehicles.controller";
 import ListVehicleController from "../controllers/Vehicle/list.vehicles.controller";
+import UpdateVehicleController from "../controllers/Vehicle/update.vehicles.controller";
 
 function vehicleRoutes(app: Express) {
   app.post("/vehicle", CreateVehicleController);
   app.get("/vehicle", ListVehicleController);
   app.delete("/vehicle/:vehicle_id", DeleteVehicleController);
+  app.patch("/vehicle/:vehicle_id", UpdateVehicleController);
 }
 
 export { vehicleRoutes };
