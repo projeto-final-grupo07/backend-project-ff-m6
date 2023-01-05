fcb95c026507e34a3bc1925e7afb6c11bc4080e1
matheuszeiser <matheuszaizer@hotmail.com>
2023-01-05 10:49:41 -0300

    Fix: returning data list of vehicles

--- src/services/Vehicle/list.vehicles.service.ts
+++ src/services/Vehicle/list.vehicles.service.ts
@@ -2,7 +2,7 @@ import prismaCliente from '../../database/prismaCliente';
 
 const ListVehicleService = async (): Promise<any> => {
   return await prismaCliente.vehicle.findMany({
-    include: { GalleryImgs: true, Message: true, user: true },
+    include: { user: true, GalleryImgs: true, Message: true },
   });
 };
 
