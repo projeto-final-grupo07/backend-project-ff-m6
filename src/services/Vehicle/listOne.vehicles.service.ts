c9756e867b87950e7214f809e7332371b9aaf1c2
matheuszeiser <matheuszaizer@hotmail.com>
2023-01-10 18:50:54 -0300

    feat: change data returning list one vehicle

--- src/services/Vehicle/listOne.vehicles.service.ts
+++ src/services/Vehicle/listOne.vehicles.service.ts
@@ -5,7 +5,11 @@ const ListOneService = async (vehicleId: string): Promise<any> => {
     where: {
       id: vehicleId,
     },
-    include: { GalleryImgs: true, Message: true },
+    include: {
+      GalleryImgs: true,
+      Message: { include: { user: { select: { name: true } } } },
+      user: true,
+    },
   });
 };
 export default ListOneService;
